a1df8ae8711daf27e3e6fc4c8fed133c2027d611
Guilherme Augusto <guilhermednts2@gmail.com>
2022-03-04 00:42:56 -0300

    'Add'

---
+++ src/screens/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {InputText, Text, } from 'react-native';
+
+
+export default function Login (){
+    return  <>
+            <InputText>Email</InputText>
+            <InputText>Password</InputText>
+            </>
+}
