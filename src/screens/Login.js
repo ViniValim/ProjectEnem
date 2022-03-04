012601ee25fc2764bdb341bcd89e9c710ab73e6a
Guilherme Augusto <guilhermednts2@gmail.com>
2022-03-04 01:05:58 -0300

    'nao'

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import {InputText, Text, } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
 
-export default function Login (){
-    return  <>
-            <InputText>Email</InputText>
-            <InputText>Password</InputText>
-            </>
-}
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    
+  },
+});
+
+export default UselessTextInput;
